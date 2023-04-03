35623724aef1dc1a1e7958fb74c7e9e21adb671f
haririthanyaa <128139401+haririthanyaa@users.noreply.github.com>
2023-04-03 09:52:32 +0530

    Add files via upload

---
+++ src/components/World.js
@@ -0,0 +1,13 @@
+function World()
+{
+    return(
+        <>
+          <header className="App-header">
+          <p>This is World Functional Component</p>
+        
+        </header>
+           
+        </>
+    );
+}
+export default World;
